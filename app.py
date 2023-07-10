2293d6a06a5dfa898b88d313a62c886126800a0f
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2023-07-11 02:48:36 +0530

    files are added to the repo

---
+++ app.py
@@ -0,0 +1,43 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def create_app():
+
+    app = Flask(__name__)
+    client = MongoClient(
+        "mongodb+srv://naveenkumardongre:2000@cluster0.xr14cww.mongodb.net/")
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.microBlog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        print([e for e in app.db.entries.find({})])
+        if request.method == 'POST':
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one(
+                {"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (entry["content"],
+             entry["date"],
+             datetime.datetime.strptime(
+                 entry["date"], "%Y-%m-%d").strftime("%b %d")
+             )
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template("home.html", entries=entries_with_date)
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=False)

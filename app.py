1dd782adc84ecbd53d08b4d9e99c2f23ca20db3b
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2023-07-11 04:11:20 +0530

    updated app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ load_dotenv()
 def create_app():
 
     app = Flask(__name__)
-    client = MongoClient(
-        "mongodb+srv://naveenkumardongre:2000@cluster0.xr14cww.mongodb.net/")
     client = MongoClient(os.getenv("MONGODB_URI"))
     app.db = client.microBlog
 
@@ -39,5 +37,5 @@ def create_app():
 
 
 # if __name__ == "__main__":
-app = create_app()
-app.run(debug=False)
+# app = create_app()
+# app.run(debug=False)
